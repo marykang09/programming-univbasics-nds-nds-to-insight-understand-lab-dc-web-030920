4725e5176e5cffed2c3cf62a28a10d234223c340
Mary Kang <mary.kang09@gmail.com>
2020-03-01 05:03:57 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,10 +10,10 @@ end
 
 
 def print_first_directors_movie_titles
-  ss_movies = directors_database[0][:movies]
+  spielberg_movies = directors_database[0][:movies]
   index = 0
 
-  while index < ss_movies.length do
+  while index < spielberg_movies.length do
     titles = ss_movies[index][:title]
     puts titles
     index +=1
